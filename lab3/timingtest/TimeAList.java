642f31c1391311bb9684689037e390f6a9c06b1e
Griffin-cyber-whatever <ru306709@gmail.com>
2024-10-27 20:44:22 +0800

    proj2

--- lab3/timingtest/TimeAList.java
+++ lab3/timingtest/TimeAList.java
@@ -24,7 +24,6 @@ public class TimeAList {
     public static void timeAListConstruction() {
         // Your times should be in seconds. You should use the Stopwatch class.
         int testRounds = 10000000;
-        Stopwatch timer = new Stopwatch();
         AList<Integer> Ns = new AList<>();
         AList<Double> times = new AList<>();
         AList<Integer> opCounts = new AList<>();
--- lab3/timingtest/TimeSLList.java
+++ lab3/timingtest/TimeSLList.java
@@ -23,6 +23,36 @@ public class TimeSLList {
 
     public static void timeGetLast() {
         // TODO: YOUR CODE HERE
+        /*Create an SLList.
+          Add N items to the SLList.
+          Start the timer.
+          Perform M getLast operations on the SLList.
+          Check the timer. This gives the total time to complete all M operations.*/
+        // M = 10000 for step 4 of the procedure described above.
+        int m = 10000;
+        int testRounds = 64000;
+        AList<Integer> Ns = new AList<>();
+        AList<Double> times = new AList<>();
+        AList<Integer> opCounts = new AList<>();
+        for (int i = 1000; i <= testRounds; i *= 2) {
+            tableDateN(i, m, Ns, times, opCounts);
+        }
+        printTimingTable(Ns, times, opCounts);
+    }
+
+    public static void tableDateN(int n, int m, AList<Integer> Ns, AList<Double> times, AList<Integer> opCounts) {
+        Stopwatch timer = new Stopwatch();
+        SLList<Integer> list = new SLList<>();
+        for (int i = 0; i < n; i++) {
+            list.addLast(1);
+        }
+        for (int i = 0; i < m; i++) {
+            list.getLast();
+        }
+        double time = timer.elapsedTime();
+        Ns.addLast(n);
+        times.addLast(time);
+        opCounts.addLast(m);
     }
 
 }
